3c1e255c5d327d20bb037370fc30d15cc1791d1c
MAC <MAC@MacBook-Emilio.local>
2022-05-04 19:52:36 +0200

    exercici_4

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -9,4 +9,17 @@ export class EscenaComponent {
 
 @Input("data") sentences:string[]=[]
 
+currentSentence:number=0
+
+prev(){
+  if ( this.currentSentence>0) this.currentSentence--
+  else this.currentSentence=3
+}
+
+next(){
+  if ( this.currentSentence<3) this.currentSentence++
+  else this.currentSentence=0
+
+}
+
 }
