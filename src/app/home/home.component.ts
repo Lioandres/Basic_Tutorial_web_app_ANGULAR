205ea55a774f9f15c6d6a6e72670d73b3bbc8353
MAC <MAC@MacBook-Emilio.local>
2022-05-12 14:23:28 +0200

    add:  .jpg route on HTML Home-Component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,20 +13,20 @@ export class HomeComponent  {
   sentences:Sentences[]=[
      {
         txt:"El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
-        img:'url(assets/img/1.jpg)'
+        img:'1.jpg'
     },
 
      {
        txt:"Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
-       img:'url(assets/img/2.jpg)'
+       img:'2.jpg'
      },
      {
       txt:"L'heroi va decidir travessar la porta que el portava a casa",
-      img:'url(assets/img/3.jpg)'
+      img:'3.jpg'
      },
      {
        txt:"Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ...",
-       img:'url(assets/img/4.jpg)'
+       img:'4.jpg'
      }
   ]
   
